e758f4a06fd1447d6ed15f9acfb958e35a85c25a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-28 12:52:45 -0500

    Change handleWeatherData function to async

--- src/requests.js
+++ src/requests.js
@@ -1,3 +1,5 @@
+import { updateForecast } from "./display";
+
 async function getWeather(location) {
     const response = await fetch(createRequestURL(location));
     const weatherData = await response.json();
@@ -13,24 +15,19 @@ function createRequestURL(location) {
     return requestURL;
 };
 
-function handleWeatherData(location) {
-    const data = getWeather(location);
-    const trimmedWeatherData = data.then(function(response) {
-        const weatherData = response.current;
-        const trimmedWeatherData = { 
-            condition: weatherData.condition,
-            temp_c: weatherData.temp_c,
-            temp_f: weatherData.temp_f,
-            feelslike_c: weatherData.feelslike_c,
-            feelslike_f: weatherData.feelslike_f,
-            humidity: weatherData.humidity,
-        }
+async function handleWeatherData(location) {
+    const data = await getWeather(location);
+    const forecast = {
+        condition: data.condition,
+        temp_c: data.temp_c,
+        temp_f: data.temp_f,
+        feelslike_c: data.feelslike_c,
+        feelslike_f: data.feelslike_f,
+        humidity: data.humidity,
+    }
 
-        return trimmedWeatherData
-    });
-
-    return trimmedWeatherData;
-}
+    updateForecast(forecast);
+};
 
 const API_BASE_URL = 'https://api.weatherapi.com/v1';
 const API_KEY = '8c3933d6332d42e9b98220128242302';

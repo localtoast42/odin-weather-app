0947c5df65ef463789b5ac0e15b9fef92e6e50a8
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-28 15:06:21 -0500

    Create function to format date in forecasts

--- src/display.js
+++ src/display.js
@@ -11,7 +11,7 @@ function createForecastDay(forecastDay) {
     forecastDayDiv.classList.add('forecast-day');
 
     const dateText = document.createElement('h2');
-    dateText.textContent = forecastDay.date;
+    dateText.textContent = formatDate(forecastDay.date);
 
     const conditionDiv = document.createElement('div');
     conditionDiv.classList.add('condition');
@@ -31,6 +31,57 @@ function createForecastDay(forecastDay) {
     forecastDayDiv.appendChild(temperatureDiv);
 
     return forecastDayDiv;
+}
+
+function formatDate(date) {
+    const splitDate = date.split('-');
+
+    let month = '';
+
+    console.log(splitDate);
+
+    switch (splitDate[1]) {
+        case '01':
+            month = 'Jan';
+            break;
+        case '02':
+            month = 'Feb';
+            break;
+        case '03':
+            month = 'Mar';
+            break;
+        case '04':
+            month = 'Apr';
+            break;
+        case '05':
+            month = 'May';
+            break;
+        case '06':
+            month = 'Jun';
+            break;
+        case '07':
+            month = 'Jul';
+            break;
+        case '08':
+            month = 'Aug';
+            break;
+        case '09':
+            month = 'Sep';
+            break;
+        case '10':
+            month = 'Oct';
+            break;
+        case '11':
+            month = 'Nov';
+            break;
+        case '12':
+            month = 'Dec';
+            break;
+    }
+
+    const dateString = `${month} ${splitDate[2]}, ${splitDate[0]}`;
+
+    return dateString;
 }
 
 const forecastDiv = document.querySelector('.forecast');

9e6d00bcd36dc711f463aa3856d844c76f9928e8
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-28 13:25:41 -0500

    Add function to generate forecast day divs

--- src/display.js
+++ src/display.js
@@ -1,17 +1,35 @@
 function updateForecast(forecast) {
     forecastDiv.textContent = '';
-    days.forEach((day) => {
-        forecastDiv.appendChild(day);
+    forecast.forEach((day) => {
+        const forecastDayDiv = createForecastDay(day);
+        forecastDiv.appendChild(forecastDayDiv);
     });
 }
 
-const forecastDiv = document.querySelector('.forecast');
-const days = [];
+function createForecastDay(forecastDay) {
+    console.log(forecastDay);
+    const forecastDayDiv = document.createElement('div');
+    const dateText = document.createElement('h1');
+    dateText.textContent = forecastDay.date;
 
-for (let i = 0; i < 3; i++) {
-    const day = document.createElement('div');
-    day.classList.add('forecast-day');
-    days.push(day);
-};
+    const conditionDiv = document.createElement('div');
+    const conditionText = document.createElement('h2');
+    const conditionImg = document.createElement('img');
+
+    conditionText.textContent = forecastDay.condition.text;
+    conditionImg.src = forecastDay.condition.icon;
+    conditionDiv.appendChild(conditionImg);
+    conditionDiv.appendChild(conditionText);
+
+    const temperatureDiv = document.createElement('div');
+
+    forecastDayDiv.appendChild(dateText);
+    forecastDayDiv.appendChild(conditionDiv);
+    forecastDayDiv.appendChild(temperatureDiv);
+
+    return forecastDayDiv;
+}
+
+const forecastDiv = document.querySelector('.forecast');
 
 export { updateForecast };

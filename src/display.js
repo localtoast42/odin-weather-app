40cd5e9266ed85d0c0356924ac19c42492403d3b
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-28 13:01:18 -0500

    Add function to generate forecast child divs

--- src/display.js
+++ src/display.js
@@ -1,5 +1,17 @@
 function updateForecast(forecast) {
-
+    forecastDiv.textContent = '';
+    days.forEach((day) => {
+        forecastDiv.appendChild(day);
+    });
 }
+
+const forecastDiv = document.querySelector('.forecast');
+const days = [];
+
+for (let i = 0; i < 3; i++) {
+    const day = document.createElement('div');
+    day.classList.add('day');
+    days.push(day);
+};
 
 export { updateForecast };
